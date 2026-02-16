2536af19f5ea1a730ce7e000e2884662265fdd9f
sunriseya777 <saayana7777@gmail.com>
2026-02-16 12:48:39 +0700

    Update lab_1.cpp

--- lab_1/lab_1.cpp
+++ lab_1/lab_1.cpp
@@ -6,45 +6,45 @@ using namespace std;
 
 int main()
 {
-    //cout << "ex 1" << endl;
-    //cout << "---------------------" << endl;
-    ////функция
-    //double x0 = 0;
-    //cout << "first arg (0,4]: ";
-    //cin >> x0;
-    //if (x0 < 0 or x0 > 4)
-    //    cout << "error";
-    //double k = (4.0 - x0) / 9.0;
-    //for (int i = 0; i < 10; i++)
-    //{
-    //    double x = x0 + i * k;
-    //    double f = sin(x) / x;
-    //    cout << x << "\t " << f << endl;
+    cout << "ex 1" << endl;
+    cout << "---------------------" << endl;
+    //функция
+    double x0 = 0;
+    cout << "first arg (0,4]: ";
+    cin >> x0;
+    if (x0 < 0 or x0 > 4)
+       cout << "error";
+    double k = (4.0 - x0) / 9.0;
+    for (int i = 0; i < 10; i++)
+    {
+       double x = x0 + i * k;
+       double f = sin(x) / x;
+       cout << x << "\t " << f << endl;
 
-    //}
-    //cout << "ex 2" << endl;
-    //cout << "---------------------" << endl;
-    ////фибоначчи
-    //int max = 0;
-    //int s = 1;
-    //int n = 2;
-    //int pred = 0;
-    //int cur = 1;
-    //int next = 0;
-    //cout << "max: ";
-    //cin >> max;
-    //while (s <= max) {
-    //    next = pred + cur;
-    //    pred = cur;
-    //    cur = next;
-    //    s = s + next;
-    //    n = n + 1;
+    }
+    cout << "ex 2" << endl;
+    cout << "---------------------" << endl;
+    //фибоначчи
+    int max = 0;
+    int s = 1;
+    int n = 2;
+    int pred = 0;
+    int cur = 1;
+    int next = 0;
+    cout << "max: ";
+    cin >> max;
+    while (s <= max) {
+       next = pred + cur;
+       pred = cur;
+       cur = next;
+       s = s + next;
+       n = n + 1;
 
 
-    //}
-    //
-    //cout << "summa: " << s << endl;
-    //cout << "number: " << n << endl;
+    }
+    
+    cout << "summa: " << s << endl;
+    cout << "number: " << n << endl;
 
     // кредиты
     double dolg;
